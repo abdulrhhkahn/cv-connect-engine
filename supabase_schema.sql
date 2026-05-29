f0912f71897829c59088999d06ed0d53b9baef42
abdulrhhkahn <abdulrhhkahn@gmail.com>
2026-05-29 20:37:05 +0500

    Add files via upload

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -183,6 +183,10 @@ create policy "Users can view their own data"
   on public.users for select
   using (auth.uid() = id);
 
+create policy "Users can insert their own data"
+  on public.users for insert
+  with check (auth.uid() = id);
+
 create policy "Users can update their own data"
   on public.users for update
   using (auth.uid() = id);
